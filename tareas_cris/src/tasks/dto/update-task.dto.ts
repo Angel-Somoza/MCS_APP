630e4f4e8d8d4f9285df3b94df6d985ab3934b6b
Angel-Somoza <somozacontrerasangeldavid@gmail.com>
2025-09-17 19:12:14 -0600

    Revert "feat(tareas_cris): CRUD NestJS + SQL Server"
    This reverts commit e686be51497f74a1eee672f441b54fe1acdd7e1d.

--- tareas_cris/src/tasks/dto/update-task.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateTaskDto } from "./create-task.dto";
-
-// Reusa los campos del create pero todos opcionales
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
