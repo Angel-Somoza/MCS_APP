1ef757b89c9a5331f4ea96e3fe0b0cd478186c91
Angel-Somoza <somozacontrerasangeldavid@gmail.com>
2025-09-23 00:13:46 -0600

    update apigateway

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -4,22 +4,9 @@ import { ClientProxy } from '@nestjs/microservices';
 @Controller()
 export class AppController {
   constructor(
-    @Inject('USERS_SERVICE') private readonly usersClient: ClientProxy,
     @Inject('COURSES_SERVICE') private readonly coursesClient: ClientProxy,
-    @Inject('PRODUCTS_SERVICE') private readonly productsClient: ClientProxy
-    
   ) {}
-
-  @Get('users')
-  getUsers() {
-    return this.usersClient.send({ cmd: 'get_users' }, {});
-  }
-
-  @Get('products')
-  getProducts() {
-    return this.productsClient.send({ cmd: 'get_products' }, {});
-  }
-
+  
 //Courses
 @Get('courses')
   getCourses() {
--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -5,9 +5,7 @@ import { AppController } from './app.controller';
 @Module({
   imports: [
     ClientsModule.register([
-      { name: 'USERS_SERVICE', transport: Transport.TCP, options: { host: 'localhost', port: 3001 } },
-      { name: 'COURSES_SERVICE', transport: Transport.TCP, options: { host: 'localhost', port: 3002 } },
-      { name: 'PRODUCTS_SERVICE', transport: Transport.TCP, options: { host: 'localhost', port: 3003 } },
+      { name: 'COURSES_SERVICE', transport: Transport.TCP, options: { host: 'localhost', port: 3002 } }
     ]),
   ],
   controllers: [AppController],
