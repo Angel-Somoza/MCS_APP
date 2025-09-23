1df5e2da1fee529f4345dc88e406517de6610456
Angel-Somoza <somozacontrerasangeldavid@gmail.com>
2025-09-23 13:52:23 -0600

    sql

--- estudiantes/src/app.module.ts
+++ estudiantes/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { EstudiantesService } from './app.service';
 import { EstudiantesController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Estudiante } from './entity/estudiante.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      database: 'school',
+      entities: [Estudiante],
+      options: {
+        encrypt: false
+      },
+     
+    }),
+    TypeOrmModule.forFeature([Estudiante])
+  ],
   controllers: [EstudiantesController],
   providers: [EstudiantesService],
 })
 export class EstudiantesModule {}
-
-
