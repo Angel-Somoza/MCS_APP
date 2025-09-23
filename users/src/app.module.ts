5b2b46948a051ad1514eb45358757d56df0d8f19
Angel-Somoza <somozacontrerasangeldavid@gmail.com>
2025-09-23 00:10:41 -0600

    Delete

--- users/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './app.controller';
-import { UsersService } from './app.service';
-
-@Module({
-  controllers: [UsersController],
-  providers: [UsersService],
-})
-export class AppModule {}
